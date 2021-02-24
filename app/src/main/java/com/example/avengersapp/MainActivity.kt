8cc8ef9b3cf907a4ab3912b1599fc1bdbe8c5b18
Nahuee <>
2021-02-24 20:10:56 -0300

    Add splash

--- app/src/main/java/com/example/avengersapp/MainActivity.kt
+++ app/src/main/java/com/example/avengersapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.avengersapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main )
+
+        setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java).apply {
+
+            }
+        }
     }
 }
